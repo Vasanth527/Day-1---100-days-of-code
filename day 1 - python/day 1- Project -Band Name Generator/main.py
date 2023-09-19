32b67aa93f9efb65aeff6bceaa48ebbf9f829b24
Vasanth <142874622+Vasanth527@users.noreply.github.com>
2023-09-19 18:49:06 +0530

    Add files via upload

---
+++ day 1 - python/day 1- Project -Band Name Generator/main.py
@@ -0,0 +1,11 @@
+#1. Create a greeting for your program.
+print("welcome to the bandname generator")
+#2. Ask the user for the city that they grew up in.
+city_name = input ("what's the name of the city you grew up in? \n")
+#3. Ask the user for the name of a pet.
+pet_name = input ("what is ypur pet name? \n")
+#4. Combine the name of their city and pet and show them their band name.
+print("your band name could be " + city_name + " "+ pet_name)
+#5. Make sure the input cursor shows on a new line:
+
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
---
+++ day 1 - python/input functions/main.py
@@ -0,0 +1,9 @@
+#This code prints the number of characters in a user's name.
+print( len( input("What is your name? ") ) )
+
+
+
+#Notes
+#If input was "Jack"
+#1st: print(len("Jack"))
+#2nd: print(4)
